c8f449555fd0a030ba8931a79f68da311188a7af
Vinícius Berger <viniciush4@gmail.com>
2020-01-16 10:12:27 -0300

    Módulo web e ajustes na api

---
+++ web/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; // especificamente para aplicações web
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
